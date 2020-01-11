80a77f723f890d2baf14bccc699fd789be1140dd
info.primemover2019 <info.primemover111@gmail.com>
2020-01-11 12:01:42 +0530

    Second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDUbp_hO6f1i5v_K0B8WsN1KYapav9MEqo",
-    authDomain: "my-portfolio-0811.firebaseapp.com",
-    databaseURL: "https://my-portfolio-0811.firebaseio.com",
-    projectId: "my-portfolio-0811",
-    storageBucket: "my-portfolio-0811.appspot.com",
-    messagingSenderId: "1078888950035",
-    appId: "1:1078888950035:web:b750a5cf3e2a7a836952e6",
-    measurementId: "G-2VLLH3YJ4T"
+    apiKey: "AIzaSyBzgG52qp5qmANbpcYastfykk8vVYIkKww",
+    authDomain: "my-portfolio-08.firebaseapp.com",
+    databaseURL: "https://my-portfolio-08.firebaseio.com",
+    projectId: "my-portfolio-08",
+    storageBucket: "my-portfolio-08.appspot.com",
+    messagingSenderId: "1048511827437",
+    appId: "1:1048511827437:web:c74333eb877668e8deabe4",
+    measurementId: "G-PSV2R458ZQ"
   }
 };
